a08ce883c443946724ee9ad72e1353132430d748
NicPWNs <nic@nicpjones.com>
2022-08-07 15:58:53 -0400

    prototype

--- src/modules/functions/streak.js
+++ src/modules/functions/streak.js
@@ -4,7 +4,6 @@ const AWS = require('aws-sdk');
 AWS.config.update({region: 'us-east-1', maxRetries: 1});
 var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
-// Define Discord Command
 exports.data = {
     name: 'streak',
     type: 1,
@@ -13,30 +12,70 @@ exports.data = {
 
 const action = async (body) => {
 
-    // Get Item
     var params = {
         TableName: 'discord-streak',
         Key: {
           id: {S: body.member.user.id}
         },
-        ProjectionExpression: 'streak'
+        ProjectionExpression: 'streak, updated, lastMid, nextMid, skipMid'
     };
 
     var data = await ddb.getItem(params).promise();
 
-    // Put Item
-    var params = {
+    var currTime = Date.now()
+    var already = false
+    var missed = false
+
+    var lastMidnight = new Date()
+    lastMidnight.setHours(0,0,0,0)
+    const est = -300; //Timezone offset for EST in minutes.
+    lastMidnight = new Date(lastMidnight.getTime() + est*60*1000)
+    lastMidnight = lastMidnight.getTime()
+
+    var nextMidnight = new Date()
+    nextMidnight.setHours(24,0,0,0)
+    nextMidnight = new Date(nextMidnight.getTime() + est*60*1000)
+    nextMidnight = nextMidnight.getTime()
+
+    var skipMidnight = new Date()
+    skipMidnight.setHours(48,0,0,0)
+    skipMidnight = new Date(skipMidnight.getTime() + est*60*1000)
+    skipMidnight = skipMidnight.getTime()
+
+    var streak = 0
+    var updated = Date.now()
+    var lastMid = lastMidnight
+    var nextMid = nextMidnight
+    var skipMid = skipMidnight
+
+    if (typeof data.Item !== 'undefined') {
+      streak = Number(data.Item.streak.S)
+      updated = Number(data.Item.updated.S)
+      lastMid = Number(data.Item.lastMid.S)
+      nextMid = Number(data.Item.nextMid.S)
+      skipMid = Number(data.Item.skipMid.S)
+    }
+
+    if (currTime > lastMid && currTime < nextMid) {
+      already = true
+    } else if (currTime > nextMid && currTime > skipMid) {
+      missed = true
+    } else if (currTime > lastMid && currTime > nextMid) {
+      var params = {
         TableName: 'discord-streak',
         Item: {
           id: {S: body.member.user.id},
-          streak: {S: String(Number(data.Item.streak.S) + 1)},
-          time: {S: String(Date.now())}
+          streak: {S: String(Number(streak) + 1)},
+          updated: {S: String(Date.now())},
+          lastMid: {S: String(lastMidnight)},
+          nextMid: {S: String(nextMidnight)},
+          skipMid: {S: String(skipMidnight)}
         }
       };
 
     await ddb.putItem(params).promise();
+    }
 
-    // Get Item
     var params = {
         TableName: 'discord-streak',
         Key: {
@@ -46,10 +85,55 @@ const action = async (body) => {
     };
 
     var data = await ddb.getItem(params).promise();
+    var streak = Number(data.Item.streak.S)
+    var emote = ""
+    var prefix = ""
 
-    // Respond to Discord
+    if (missed) {
+      prefix = "You missed your streak! "
+    }
+
+    if (already) {
+      prefix = "You've already done your streak today! "
+    }
+
+    switch (true) {
+      case (streak < 2):
+        emote = "💩";
+        break;
+      case (streak < 3):
+        emote = "✌️";
+        break;
+      case (streak < 4):
+        emote = "👌";
+        break;
+      case (streak < 5):
+        emote = "🍀";
+        break;
+      case (streak < 10):
+        emote = "🔥";
+        break;
+      case (streak < 25):
+        emote = "🧨";
+        break;
+      case (streak < 50):
+        emote = "🏆";
+        break;
+      case (streak < 75):
+        emote = "💀";
+        break;
+      case (streak < 100):
+        emote = "💎";
+        break;
+      case (streak < 1000):
+        emote = "💯 You weren't supposed to actually take it this far...";
+        break;
+      default:
+        emote = "🔥";
+    }
+
     var response = {
-        "content": "Your streak is: " + data.Item.streak.S
+        "content": prefix + "Your streak is: " + streak + " " + emote
     }
 
     return response
